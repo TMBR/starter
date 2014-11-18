80d88433e23f4b99878949e6600e604bf2cc6eb9
Matt Richmond <mrichmond@wearetmbr.com>
2014-11-18 10:55:28 -0800

    we’re not using coffee

--- wp-content/themes/tmbr/gulp/tasks/uglify_app.js
+++ wp-content/themes/tmbr/gulp/tasks/uglify_app.js
@@ -10,7 +10,7 @@
   rename = require("gulp-rename");
 
   module.exports = function() {
-    return gulp.task("uglify_app", ["coffee"], function() {
+    return gulp.task("uglify_app", function() {
       return gulp.src("public/js/application.js").pipe(uglify()).pipe(rename("application.min.js")).pipe(gulp.dest("public/js"));
     });
   };
