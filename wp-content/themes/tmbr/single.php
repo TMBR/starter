93a1d14964c16f5aff4e15ea14c73b3e8861311e
bmodena <bmodena@sussind.com>
2015-02-19 13:26:38 -0700

    update to 4.1.1

--- wp-content/themes/tmbr/single.php
+++ wp-content/themes/tmbr/single.php
@@ -1,27 +1,27 @@
 <?php get_header(); ?>
 
-<div id="page-wrapper">	
+<div id="page-wrapper">
 	<div class="container">
 		<div class="row">
-			
-			<div class="col-sm-8">		
-			
+
+			<div class="col-sm-8">
+
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				
+
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
-				
+
 				<?php edit_post_link(); ?>
 				<?php comments_template(); ?>
-				
+
 				<?php endwhile; endif; ?>
-				
-				<?php get_template_part('partials/single-nav'); ?>
-				
+
+				<?php get_template_part('partials/post/single-nav'); ?>
+
 			</div><!-- /col -->
 
 		<?php get_sidebar(); ?>
-		
+
 		</div><!-- /row -->
 	</div><!-- /container -->
 </div><!-- #page-wrapper -->
