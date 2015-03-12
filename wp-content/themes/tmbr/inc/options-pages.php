802705c113c8d31f926db28ea268c6a68f57cd0d
Galen Strasen <galen@wearetmbr.com>
2015-03-12 11:30:03 -0600

    ACF Options page include

--- wp-content/themes/tmbr/inc/options-pages.php
+++ wp-content/themes/tmbr/inc/options-pages.php
@@ -1,16 +1,34 @@
 <?php
 
+// ACF PRO Options pages
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	/*
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	*/
+}
+
+
 // Creates multiple Options Pages with ACF Options Page Add-On
 // http://www.advancedcustomfields.com/resources/filters/acfoptions_pagesettings/
 
 function my_acf_options_page_settings( $settings ) {
 	 $settings['title'] = 'Options';
-	 $settings['pages'] = array('Option Page #1', 'Option Page #2');
+	 $settings['pages'] = array('Call to Action', 'Slices', 'Footer');
 
 	 return $settings;
 }
 
 add_filter('acf/options_page/settings', 'my_acf_options_page_settings');
-
-
-?>
--- wp-content/themes/tmbr/inc/theme-setup.php
+++ wp-content/themes/tmbr/inc/theme-setup.php
@@ -45,25 +45,6 @@ function add_ie_html5_shim () {
 add_action('wp_head', 'add_ie_html5_shim');
 
 
-// ACF PRO Options pages
-if( function_exists('acf_add_options_page') ) {
-
-	acf_add_options_page(array(
-		'page_title' 	=> 'Theme General Settings',
-		'menu_title'	=> 'Theme Settings',
-		'menu_slug' 	=> 'theme-general-settings',
-		'capability'	=> 'edit_posts',
-		'redirect'		=> false
-	));
-
-	/*
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Theme Header Settings',
-		'menu_title'	=> 'Header',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-	*/
-}
 
 
 function tmbr_widgets_init() {
