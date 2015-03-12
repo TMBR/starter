254eb2fc3203bf54d83d348b1f6e5739f1668c58
Galen Strasen <galen@wearetmbr.com>
2015-03-12 11:27:06 -0600

    Updated custom-img-size with syntax varieties

--- wp-content/themes/tmbr/inc/custom-img-size.php
+++ wp-content/themes/tmbr/inc/custom-img-size.php
@@ -1,6 +1,9 @@
 <?php
 
 if ( function_exists( 'add_image_size' ) ) {
-	add_image_size( 'full_screen', 1680, 945, true ); //(cropped)
-	add_image_size( 'mobile_full_screen', 1170, 658, true ); //(cropped)
+	add_image_size( 'full_screen', 1680, 1000 ); // Ideal dimensions for load times on full width images
+	add_image_size( 'xlarge', 1200, 1200 ); 
+
+	add_image_size( 'lg_thumb', 700, 700, true ); // Force crop
+	// add_image_size( 'headshot', 600, 450, array('center','top') ); > Set where image crops from
 }
