46a18722e6c22b767bccce7351e6612bf21575cb
Matt Richmond <mrichmond@wearetmbr.com>
2015-03-12 16:28:06 -0600

    db password update

--- wp-config.php
+++ wp-config.php
@@ -9,7 +9,7 @@ define( 'WP_DEFAULT_THEME', 'tmbr' );
 define( 'WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] );
 define( 'WP_SITEURL', WP_HOME . '/' );
 define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
