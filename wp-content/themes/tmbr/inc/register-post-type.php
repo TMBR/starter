202342ae6b063a00094662f5e1256e00d84cdea7
bmodena <bmodena@sussind.com>
2015-03-24 10:23:16 -0600

    mixin udpates

--- wp-content/themes/tmbr/inc/register-post-type.php
+++ wp-content/themes/tmbr/inc/register-post-type.php
@@ -8,76 +8,69 @@ add_action( 'init', 'create_post_type' );
 	function create_post_type() {
 
 
-	register_post_type( 'clients',
-			array (	'label' => 'Clients',
+	register_post_type( 'packages',
+			array (	'label' => 'Packages',
+				'description' => 'Packages and Specials',
+				'public' => true,
+				'show_ui' => true,
+				'show_in_menu' => true,
+				'capability_type' => 'post',
+				'hierarchical' => true,
+				'has_archive' => true,
+				'rewrite' => true,
+				'query_var' => true,
+				'supports' => array('title','editor','thumbnail','page-attributes'),
+				'taxonomies' => array(),
+				'menu_icon' => 'dashicons-megaphone',
+
+				'labels' =>
+					array (
+	  					'name' => 'Packages', /* This is the Title of the Group */
+	  					'singular_name' => 'Package', /* This is the individual type */
+						'menu_name' => 'Packages', /* The add new menu item */
+						'add_new' => 'Add Package', /* Add New Display Title */
+						'add_new_item' => 'Add New Package',
+						'edit' => 'Edit', /* Edit Dialog */
+						'edit_item' => 'Edit Package', /* Edit Display Title */
+						'new_item' => 'New Package', /* New Display Title */
+						'view_item' => 'View Package', /* View Display Title */
+						'search_items' => 'Search Packages', /* Search Custom Type Title */
+						'not_found' => 'No Packages Found', /* This displays if there are no entries yet */
+						'not_found_in_trash' => 'No Packages Found in Trash' /* This displays if there is nothing in the trash */
+						),
+			)
+	); // End Packages Post type
+
+	register_post_type( 'activities',
+			array (	'label' => 'Activities',
 				'description' => '',
 				'public' => true,
 				'show_ui' => true,
 				'show_in_menu' => true,
 				'capability_type' => 'post',
-				'hierarchical' => false,
+				'hierarchical' => true,
+				'has_archive' => true,
 				'rewrite' => true,
 				'query_var' => true,
-				'supports' => array('title','thumbnail'),
+				'supports' => array('title','editor','thumbnail','page-attributes'),
 				'taxonomies' => array(),
+				'menu_icon' => 'dashicons-location-alt',
+
 				'labels' =>
 					array (
-	  					'name' => 'Clients', /* This is the Title of the Group */
-	  					'singular_name' => 'Client', /* This is the individual type */
-						'menu_name' => 'Clients', /* The add new menu item */
-						'add_new' => 'Add Client', /* Add New Display Title */
-						'add_new_item' => 'Add New Client',
+	  					'name' => 'Activities', /* This is the Title of the Group */
+	  					'singular_name' => 'Activity', /* This is the individual type */
+						'menu_name' => 'Activities', /* The add new menu item */
+						'add_new' => 'Add Activity', /* Add New Display Title */
+						'add_new_item' => 'Add New Activity',
 						'edit' => 'Edit', /* Edit Dialog */
-						'edit_item' => 'Edit Client', /* Edit Display Title */
-						'new_item' => 'New Client', /* New Display Title */
-						'view_item' => 'View Client', /* View Display Title */
-						'search_items' => 'Search Clients', /* Search Custom Type Title */
-						'not_found' => 'No Clients Found', /* This displays if there are no entries yet */
-						'not_found_in_trash' => 'No Clients Found in Trash' /* This displays if there is nothing in the trash */
+						'edit_item' => 'Edit Activity', /* Edit Display Title */
+						'new_item' => 'New Activity', /* New Display Title */
+						'view_item' => 'View Activity', /* View Display Title */
+						'search_items' => 'Search Activities', /* Search Custom Type Title */
+						'not_found' => 'No Activities Found', /* This displays if there are no entries yet */
+						'not_found_in_trash' => 'No Activities Found in Trash' /* This displays if there is nothing in the trash */
 						),
-				)
-			);
-		?>
-
-		<!--
-			register_post_type( 'snow_reports',
-			array (	'label' => 'Snow Reports',
-			'description' => '',
-			'public' => false,
-			'show_ui' => true,
-			'show_in_menu' => true,
-			'capability_type' => 'post',
-			'hierarchical' => false,
-			'supports' => false,
-			'taxonomies' => array(),
-			'labels' =>
-				array (
-				'name' => 'Snow Reports', /* This is the Title of the Group */
-				'singular_name' => 'Snow Report', /* This is the individual type */
-				'menu_name' => 'Snow Reports', /* The add new menu item */
-				'add_new' => 'Add Snow Report', /* Add New Display Title */
-				'add_new_item' => 'Add New Snow Report',
-				'edit' => 'Edit', /* Edit Dialog */
-				'edit_item' => 'Edit Snow Reports', /* Edit Display Title */
-				'new_item' => 'New Snow Report', /* New Display Title */
-				'view_item' => 'View Snow Reports', /* View Display Title */
-				'search_items' => 'Search Snow Reports', /* Search Custom Type Title */
-				'not_found' => 'No Snow Reports Found', /* This displays if there are no entries yet */
-				'not_found_in_trash' => 'No Snow Reports Found in Trash' /* This displays if there is nothing in the trash */
-				),
 			)
-		);
-		-->
-
-		<?php
-
-			/* this ads your post categories to your custom post type */
-			register_taxonomy_for_object_type('category', 'clients');
-
-			/* this ads your post tags to your custom post type */
-			register_taxonomy_for_object_type('post_tag', 'clients');
-
+	); // End Activities
 }
-
-
-?>
