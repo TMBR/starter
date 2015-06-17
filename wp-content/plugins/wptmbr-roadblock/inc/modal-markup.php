c088bfc65256eb5df6680df8a8605a73df6bf731
Galen Strasen <galen@wearetmbr.com>
2015-06-17 09:57:54 -0700

    Markup & only output if gravity form function exists

--- wp-content/plugins/wptmbr-roadblock/inc/modal-markup.php
+++ wp-content/plugins/wptmbr-roadblock/inc/modal-markup.php
@@ -1,14 +1,26 @@
+
+<?php 
+if( !function_exists('gravity_form') ) {  
+	return; 
+} 
+?>
+
 <div class="overlay"></div>
   <div id="newsletter_signup" class="modal">
 	  <div class="popup-bg"></div> 
-	  <h3>E-mail Newsletter Sign Up</h3>
-	  <?php $options = get_option( 'wptmbr_roadblock'); print_r($options);?>
+	  <h3><?php echo esc_html($wptmbr_header); ?></h3>
+	  <?php //$options = get_option( 'wptmbr_roadblock'); print_r($options);?>
 	  	<div id="email-signup">
 	  	<p style="margin-bottom: 0;">Receive early notification of packages, last minute deals and upcoming events.</p>
 	  	<?php //echo do_shortcode('[gravityform id='. $wptmbr_gfid.' title=false description=false ajax=true]'); ?>
-	  	<?php $test = '[gravityform id='. $wptmbr_gfid.' title=false description=false ajax=true]';
-	  	echo $test; ?>
+
+	  	
+	  	<?php gravity_form( intval($wptmbr_gfid), false, false, false, '', true); ?>
+	  	
+	  	<?php //$test = '[gravityform id='. $wptmbr_gfid .' title=false description=false ajax=true]';
+	  	//echo $test; ?>
 		  	<div class="clear"></div>
 		  	<p class="close-email">&times; Close Window</p>
 		</div>
   </div>
+
