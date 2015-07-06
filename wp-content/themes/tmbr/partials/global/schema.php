611b91bf2fe1eaaa83aacf5b330812bcd3455ae9
Galen Strasen <galen@wearetmbr.com>
2015-07-06 12:06:26 -0600

    Created schema partial

---
+++ wp-content/themes/tmbr/partials/global/schema.php
@@ -0,0 +1,27 @@
+<div itemscope itemtype="http://schema.org/LocalBusiness">
+
+				<h4><span itemprop="name"></span></h4>
+
+                      <a itemprop="url" href="grandtarghee.com"><div id="footer-logo" itemprop="name">Grand Targhee Resort</div></a>
+                      <div class="description" itemprop="description">Grand Targhee Ski Resort, Located in Alta Wyoming</div>
+
+                      <div class="row">
+
+                        <div class="col-md-12 col-sm-6 col-xs-12">
+                          <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                            <span itemprop="streetAddress">3300 Ski Hill Rd</span><br>
+                            <span itemprop="addressLocality">Alta</span>,
+                            <span itemprop="addressRegion">WY</span>
+                            <span itemprop="postalCode">83414</span><br>
+                          </div><!-- /address -->
+                        </div><!-- /col -->
+
+                        <div class="col-md-12 col-sm-6 col-xs-12">
+                          <span itemprop="telephone"> <i class="fa fa-phone"></i> 307.353.2300</span> <br />
+                          <span itemprop="telephone"> <i class="fa fa-phone"></i> 800.TARGHEE</span> <br />
+                          <a href="mailto:info@GrandTarghee.com" itemprop="email">info@GrandTarghee.com</a>
+                       </div><!-- /col -->
+
+                      </div><!-- /row -->
+
+                </div><!-- /contact-wrap -->
