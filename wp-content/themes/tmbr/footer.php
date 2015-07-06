f075ea06ed436df788fc87a0ed6732cf9435c728
Galen Strasen <galen@wearetmbr.com>
2015-07-06 12:04:02 -0600

    Copied schema stuff from targhee

--- wp-content/themes/tmbr/footer.php
+++ wp-content/themes/tmbr/footer.php
@@ -2,7 +2,35 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
 	<div class="container">
 		<div class="row">
-			<div class="site-info col-xs-12">
+			<div class="col-xs-6">
+				<div itemscope itemtype="http://schema.org/LocalBusiness">
+
+						
+                      <a itemprop="url" href="grandtarghee.com"><div id="footer-logo" itemprop="name">Grand Targhee Resort</div></a>
+                      <div class="description" itemprop="description">Grand Targhee Ski Resort, Located in Alta Wyoming</div>
+
+                      <div class="row">
+
+                        <div class="col-md-12 col-sm-6 col-xs-12">
+                          <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                            <span itemprop="streetAddress">3300 Ski Hill Rd</span><br>
+                            <span itemprop="addressLocality">Alta</span>,
+                            <span itemprop="addressRegion">WY</span>
+                            <span itemprop="postalCode">83414</span><br>
+                          </div><!-- /address -->
+                        </div><!-- /col -->
+
+                        <div class="col-md-12 col-sm-6 col-xs-12">
+                          <span itemprop="telephone"> <i class="fa fa-phone"></i> 307.353.2300</span> <br />
+                          <span itemprop="telephone"> <i class="fa fa-phone"></i> 800.TARGHEE</span> <br />
+                          <a href="mailto:info@GrandTarghee.com" itemprop="email">info@GrandTarghee.com</a>
+                       </div><!-- /col -->
+
+                      </div><!-- /row -->
+
+                </div><!-- /contact-wrap -->
+			</div>
+			<div class="col-xs-6">
 				Site By
 				<span class="sep"> | </span>
 				<a href="http://wearetmbr.com/" rel="designer">TMBR</a>
