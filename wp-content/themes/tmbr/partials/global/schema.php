209b49fc94523e156c9fd160c19efa5a6f9a43ae
Galen Strasen <galen@wearetmbr.com>
2015-07-06 13:27:44 -0600

    Schema markup

--- wp-content/themes/tmbr/partials/global/schema.php
+++ wp-content/themes/tmbr/partials/global/schema.php
@@ -1,27 +1,89 @@
+<?php
+
+$name = get_field('schema_name','option');
+$description = get_field('schema_description','option');
+$logo = get_field('schema_logo','option');
+$url = get_field('schema_url','option');
+
+$streetAddress = get_field('schema_street_address','option');
+$poBox = get_field('schema_po_box','option');
+$city = get_field('schema_city','option');
+$state = get_field('schema_state','option');
+$country = get_field('schema_country','option');
+$zip = get_field('schema_zip','option');
+
+$phone = get_field('schema_phone','option');
+$fax = get_field('schema_fax','option');
+$email = get_field('schema_email','option');
+
+?>
+
+
 <div itemscope itemtype="http://schema.org/LocalBusiness">
 
-				<h4><span itemprop="name"></span></h4>
+  <?php 
+  // BUSINESS INFORMATION
+  if ( $name ) { ?> 
+  <h4><span itemprop="name"><?php echo $name; ?></span></h4>
+  <?php } 
 
-                      <a itemprop="url" href="grandtarghee.com"><div id="footer-logo" itemprop="name">Grand Targhee Resort</div></a>
-                      <div class="description" itemprop="description">Grand Targhee Ski Resort, Located in Alta Wyoming</div>
+  if ( $description ) { ?> 
+  <div itemprop="description"><?php echo $description; ?></div>
+  <?php } 
 
-                      <div class="row">
+  if ( $logo ) { ?> 
+  <img itemprop="logo" src="<?php echo $logo['sizes']['thumbnail']; ?>" alt="<?php echo $logo['alt']; ?>" class="img-responsive" />
+  <?php } 
 
-                        <div class="col-md-12 col-sm-6 col-xs-12">
-                          <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                            <span itemprop="streetAddress">3300 Ski Hill Rd</span><br>
-                            <span itemprop="addressLocality">Alta</span>,
-                            <span itemprop="addressRegion">WY</span>
-                            <span itemprop="postalCode">83414</span><br>
-                          </div><!-- /address -->
-                        </div><!-- /col -->
+  if ( $url ) { ?> 
+  <a itemprop="url" href="grandtarghee.com"><?php echo $url; ?></a>
 
-                        <div class="col-md-12 col-sm-6 col-xs-12">
-                          <span itemprop="telephone"> <i class="fa fa-phone"></i> 307.353.2300</span> <br />
-                          <span itemprop="telephone"> <i class="fa fa-phone"></i> 800.TARGHEE</span> <br />
-                          <a href="mailto:info@GrandTarghee.com" itemprop="email">info@GrandTarghee.com</a>
-                       </div><!-- /col -->
+  <?php } ?>
 
-                      </div><!-- /row -->
+  <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+    <?php 
+    // STREET ADDRESS
+    if ( $streetAddress ) { ?> 
+    <span itemprop="streetAddress"><?php echo $streetAddress; ?></span><br/>
+    <?php } 
 
-                </div><!-- /contact-wrap -->
+    if ( $poBox ) { ?> 
+    <span itemprop="postOfficeBoxNumber">PO Box <?php echo $poBox; ?></span><br/>
+    <?php } 
+
+    if ( $city ) { ?> 
+    <span itemprop="addressLocality"><?php echo $city; ?></span>,
+    <?php } 
+
+    if ( $state) { ?> 
+    <span itemprop="addressRegion"><?php echo $state; ?></span><br/>
+    <?php } 
+
+    if ( $country ) { ?> 
+    <span itemprop="addressCountry"><?php echo $country; ?></span></br/>
+    <?php } 
+
+    if ( $zip ) { ?> 
+    <span itemprop="postalCode"><?php echo $zip; ?></span>
+    
+    <?php } ?>
+  </div><!-- /address -->
+
+  <?php 
+  // STREET ADDRESS
+  if ( $phone ) { ?> 
+  <span itemprop="telephone"><?php echo $phone; ?></span> <br />
+  <?php } 
+
+  if ( $fax ) { ?> 
+  <span itemprop="faxNumber"><?php echo $fax; ?></span> <br />
+  <?php } 
+
+  if ( $email ) { ?> 
+  <a href="mailto:<?php echo $email; ?>" itemprop="email"><?php echo $email; ?></a>
+  <?php } ?>
+
+  </div><!-- /schema.org -->
+
+
+                      
