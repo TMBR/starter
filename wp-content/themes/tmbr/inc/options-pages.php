3abb47486b4e5457afbf89f6ced3b52b3a7c4b87
Galen Strasen <galen@wearetmbr.com>
2015-07-06 11:30:20 -0600

    Renamed options page

--- wp-content/themes/tmbr/inc/options-pages.php
+++ wp-content/themes/tmbr/inc/options-pages.php
@@ -4,9 +4,9 @@
 if( function_exists('acf_add_options_page') ) {
 
 	acf_add_options_page(array(
-		'page_title' 	=> 'Theme General Settings',
-		'menu_title'	=> 'Theme Settings',
-		'menu_slug' 	=> 'theme-general-settings',
+		'page_title' 	=> 'Global Settings',
+		'menu_title'	=> 'Global Settings',
+		'menu_slug' 	=> 'global-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
 	));
