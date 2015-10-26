36ccc75acceedba9efa0eb46623e44547b3fd5a5
Galen Strasen <galen@wearetmbr.com>
2015-10-26 16:41:07 -0700

    Add classes to html based on browser

--- wp-content/themes/tmbr/header.php
+++ wp-content/themes/tmbr/header.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!--[if lt IE 7]><html class="ie ie6" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 7]><html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]><html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 9]><html class="ie9" <?php language_attributes(); ?>><![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
   <head>
 
     <meta charset="<?php bloginfo('charset'); ?>" />
