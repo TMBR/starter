cf445fef7000ad31835cd2715633c79d11837d8d
Galen Strasen <galen@wearetmbr.com>
2015-10-27 14:07:26 -0700

    Packages

--- wp-content/themes/tmbr/gulpfile.js
+++ wp-content/themes/tmbr/gulpfile.js
@@ -57,8 +57,8 @@
       // 'assets/vendor/pace/pace.js',                                      PACE (page load progress bar) - http://github.hubspot.com/pace/docs/welcome/
       'assets/vendor/wow/dist/wow.js',                                  //  WOW.js (animate asset loading) *depends on animate.css - http://mynameismatthieu.com/WOW/
       // 'assets/vendor/videojs/dist/video-js/video.js',                    Video.js (work with HTML5 video) - http://videojs.com/
-      'assets/vendor/respond/src/respond.js'                           //   Respond.js (media query helper for IE) - https://github.com/scottjehl/Respond
-      // 'assets/vendor/responsejs/response.js'                             Response.js (serve responsive content via HTML5) - http://responsejs.com/
+      'assets/vendor/respond/src/respond.js',                           //  Respond.js (media query helper for IE) - https://github.com/scottjehl/Respond
+      'assets/vendor/responsejs/response.js'                            //  Response.js (serve responsive content via HTML5) - http://responsejs.com/
 
       
     ],
