05cb93123d58f22585f63e0147b4df8a1beba27a
Galen Strasen <galen@wearetmbr.com>
2015-12-04 14:50:01 -0800

    Function cleanup

--- wp-content/themes/tmbr/functions.php
+++ wp-content/themes/tmbr/functions.php
@@ -3,57 +3,32 @@
 // TMBR Helpers - always include this first, so other files can rely on it
 get_template_part( 'inc/helpers' );
 
-// AUTO-UPDATES
-//get_template_part( 'inc/auto-updates' );
-
-// WEATHER STUFF
-// get_template_part( 'inc/weather/init' );
-// get_template_part( 'inc/weather-overrides' );
-
-// SOCIAL FEED
-// get_template_part( 'inc/socialfeed/init' );
-
-// THEME SETUP
+// Theme Setup & Enqueue
 get_template_part( 'inc/theme-setup' );
-
-// ENQUEUE SCRIPTS & STYLES
 get_template_part( 'inc/load-scripts' );
 
-// PAGE EXCERPT
+
+// Hide Admin Bar for Dev Purposes - this can also be done via Dashboard in WP Engine options
+add_filter( 'show_admin_bar', '__return_false' );
+
+
+// Theme Supports
 get_template_part( 'inc/page-excerpt' );
-
-// REGISTER CUSTOM POST TYPE
-// get_template_part( 'inc/register-post-type' );
-
-// REGISTER CUSTOM TAXONOMY
-// get_template_part( 'inc/register-taxonomy' );
-
-// REGISTER SIDEBARS
-// get_template_part( 'inc/register-sidebars' );
-
-// REGISTER MENUS
-get_template_part( 'inc/register-menus' );
-
-// CUSTOM IMG SIZE
-get_template_part( 'inc/custom-img-size' );
-
-// DEFINE CATEGORY OR OTHER TERMS
-// @define( 'cat_featured', 17 );
-
-// FAMILY TREE
-// get_template_part( 'inc/is-tree' );
-
-// Custom Gravity Form Submit filters
-//get_template_part( 'inc/gravity-form-filters' );
-
-// HIDES ADMIN BAR FOR DEV PURPOSES - this can also be done via Dashboard in WP Engine options
- add_filter( 'show_admin_bar', '__return_false' );
-
-// ACF - Add Multiple Options Pages (you need to have the ACF Options Page Add-On installed
 get_template_part( 'inc/options-pages' );
 
-// SHORTCODES
+// Customization
+
+// get_template_part( 'inc/register-post-type' );
+// get_template_part( 'inc/register-taxonomy' );
+// get_template_part( 'inc/register-sidebars' );
+get_template_part( 'inc/register-menus' );
+get_template_part( 'inc/custom-img-size' );
+
+// Functions
+// @define( 'cat_featured', 17 ); // define cat or other terms
+// get_template_part( 'inc/is-tree' ); // is tree relation
 // get_template_part( 'inc/shortcodes' );
 
-// CUSTOM DASHBOARD WIDGET
-// get_template_part( 'inc/dashboard-widget' );
+
+
+
