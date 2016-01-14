c14210818c570fc2a56c849d5e4b4b89e09878dd
bmodena <bmodena@sussind.com>
2016-01-14 08:56:03 -0700

    update image size in sldier

--- wp-content/themes/tmbr/partials/slider/home-hero.php
+++ wp-content/themes/tmbr/partials/slider/home-hero.php
@@ -4,9 +4,9 @@
 
   <ul class="slides">
 
-    <?php 
+    <?php
 
-    if ( get_field( 'hp_slideshow' ) ) { 
+    if ( get_field( 'hp_slideshow' ) ) {
       while(has_sub_field('hp_slideshow')) {
 
       $image = get_sub_field('hp_image');
@@ -14,27 +14,27 @@
       $text = get_sub_field('hp_text');
       $btnTxt = get_sub_field('hp_button_text');
       $btnLink = get_sub_field('hp_button_link'); ?>
-    
+
     <li>
 
-      <img src="<?php echo $image['url'];?>" alt="<?php echo $image['alt'];?>"/>
+      <img src="<?php echo $image['sizes']['full_screen'];?>" alt="<?php echo $image['alt'];?>"/>
 
       <div class="caption">
         <?php if ( $header ) { ?>
           <h2><?php echo $header; ?></h2>
-        <?php } 
+        <?php }
 
         if ( $text ) { ?>
           <p><?php echo $text; ?></p>
         <?php }
 
-        if ( $btnTxt ) { ?> 
+        if ( $btnTxt ) { ?>
           <a href="<?php echo $btnLink; ?>" class="btn"><?php echo $btnTxt; ?></a>
         <?php } ?>
       </div><!-- /caption -->
-      
+
     </li>
-    
+
   <?php } } else { ?>
 
     <li>
@@ -51,7 +51,7 @@
     </li>
 
   <?php } ?>
-  
+
   </ul>
-  
+
 </div><!-- #home-slider -->
