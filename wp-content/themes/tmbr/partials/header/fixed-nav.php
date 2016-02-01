c8bf5430f057fd348dc85cf4ab40f29b5ecaad9d
bmodena <bmodena@sussind.com>
2016-02-01 13:38:00 -0700

    nav updates

--- wp-content/themes/tmbr/partials/header/fixed-nav.php
+++ wp-content/themes/tmbr/partials/header/fixed-nav.php
@@ -3,7 +3,7 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar one"></span>
                 <span class="icon-bar two"></span>
