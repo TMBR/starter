cdad92dfe734445730be3c709de34a38a1f7989b
Galen Strasen <galen@wearetmbr.com>
2016-07-07 12:58:57 -0600

    Media component partials

---
+++ wp-content/themes/tmbr/new-partials/components/media/img-bg.php
@@ -0,0 +1,3 @@
+
+
+<div class="img-bg" style="background-image: url(<?php echo $img['sizes'][$size]; ?>);"></div>
---
+++ wp-content/themes/tmbr/new-partials/components/media/video.php
@@ -0,0 +1,20 @@
+
+<?php
+$url = $url;
+$parsedVidURL = parse_url($url);
+$vidPART = $parsedVidURL['path'];
+$vidPART = str_replace('/','',$vidPART);
+$hash = simplexml_load_file("https://vimeo.com/api/v2/video/$vidPART.xml");
+$smUrl =  $hash->video[0]->thumbnail_large;
+//$image = $smUrl;
+
+if(!empty($img)) {
+  $vid_img = $img['sizes'][$size];
+}
+else {
+  $vid_img = $smUrl;
+}
+?>
+
+<div class="img-bg" style="background-image: url('<?php echo $vid_img; ?>');"></div>
+<a href="<?php echo $url; ?>" class="popup-video js-popup-video"><span class="trigger"></span></a>
