78f94bfff96649f43119dcb397e367034b2a642e
Galen Strasen <galen@wearetmbr.com>
2016-07-12 15:48:36 -0600

    Moved new partials

--- wp-content/themes/tmbr/partials/custom-fields/flex/content/text-content.php
+++ wp-content/themes/tmbr/partials/custom-fields/flex/content/text-content.php
@@ -9,7 +9,7 @@ if($layout == 'default') {
   $align = get_sub_field('ac_tc_default_align');
   $content_1col = get_sub_field('ac_tc_default_content');
 
-  tmbr_load_template( 'new-partials/layout/default.php', array(
+  tmbr_load_template( 'partials/layout/default.php', array(
     'align' => (isset($align)) ? $align : null,
     'content' => (isset($content_1col)) ? $content_1col : null
   ));
@@ -26,13 +26,13 @@ elseif($layout == 'text_img') {
   $right_text = get_sub_field('ac_tc_right_text_split');
 
   if($img_align == 'left_img') {
-    tmbr_load_template( 'new-partials/layout/left-img.php', array(
+    tmbr_load_template( 'partials/layout/left-img.php', array(
       'img' => (isset($img_split)) ? $img_split : null,
       'content' => (isset($right_text)) ? $right_text : null
     ));
   }
   elseif($img_align == 'right_img') {
-    tmbr_load_template( 'new-partials/layout/right-img.php', array(
+    tmbr_load_template( 'partials/layout/right-img.php', array(
       'img' => (isset($img_split)) ? $img_split : null,
       'content' => (isset($left_text)) ? $left_text : null
     ));
@@ -50,7 +50,7 @@ elseif($layout == '2_col') {
   $left_text = get_sub_field('ac_tc_left_text_split');
   $right_text = get_sub_field('ac_tc_right_text_split');
 
-  tmbr_load_template( 'new-partials/layout/2-col.php', array(
+  tmbr_load_template( 'partials/layout/2-col.php', array(
     'l_align' => (isset($l_align_2)) ? $l_align_2 : null,
     'r_align' => (isset($r_align_2)) ? $r_align_2 : null,
     'l_content' => (isset($left_text)) ? $left_text : null,
@@ -68,7 +68,7 @@ elseif($layout == '3_col') {
   $center_text_3 = get_sub_field('ac_tc_center_text_3col');
   $right_text_3= get_sub_field('ac_tc_right_text_3col');
 
-  tmbr_load_template( 'new-partials/layout/3-col.php', array(
+  tmbr_load_template( 'partials/layout/3-col.php', array(
     'l_content' => (isset($left_text_3)) ? $left_text_3 : null,
     'c_content' => (isset($center_text_3)) ? $center_text_3 : null,
     'r_content' => (isset($right_text_3)) ? $right_text_3 : null
