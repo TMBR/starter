68c0ecfee92b92c10c99f059af6171ba4ee8ab16
Galen Strasen <galen@wearetmbr.com>
2016-10-18 14:36:55 -0700

    ACF link card and button module

---
+++ wp-content/themes/tmbr/partials/01_atom/acf-button.php
@@ -0,0 +1,11 @@
+<?php
+
+$text = get_field('btn_text');
+$type = get_field('btn_type'); // page / section / ext / file
+
+$page = get_field('btn_pg_link');
+$ext_url = get_field('btn_url');
+$file = get_field('btn_file');
+$sec_id = get_field('btn_sec_id');
+
+?>
