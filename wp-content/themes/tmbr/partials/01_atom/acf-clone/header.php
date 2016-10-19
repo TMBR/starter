155bcd241fabee34691b9ff608362b2c4a5190d9
Galen Strasen <galen@wearetmbr.com>
2016-10-19 13:29:01 -0700

    Header module

--- wp-content/themes/tmbr/partials/01_atom/acf-clone/header.php
+++ wp-content/themes/tmbr/partials/01_atom/acf-clone/header.php
@@ -1,18 +1,29 @@
 <?php
-/* Required vars:
-* $tag
-* $align
-* $text
-*
-* Used by:
-* partials/fields/flex/content.php
-* partials/fields/flex/cards.php
-*/
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+global $post;
 
+/**
+ *  ACF - BUTTON (clone field)
+ *
+ *  Checks if variables to get_fields are set in load template,
+ *  otherwise sets vars to base button fields
+ *
+ *  Required vars:
+ *   $el
+ *   $align
+ *   $text
+ */
+
+
+$el = isset($el) ? $el : get_field('header_style');
+$align = isset($align) ? $align : get_field('header_align');
+$text = isset($text) ? $text : get_field('header_text');
 ?>
 
-<<?php if(isset($tag)) { echo $tag; } else { echo 'h3'; } ?> class="<?php if(isset($align)) { echo 'text-'.$align; } ?>"> <!-- h1 / h2 / h3 / h4 / h5 -->
+<<?php if(isset($el)) { echo $el; } else { echo 'h3'; } ?> class="<?php if(isset($align)) { echo 'text-'.$align; } ?>">
 
-  <?php if(!empty($text)) {echo $text;} ?>
+  <?php if(!empty($text)) { echo $text; } ?>
 
-</<?php if(isset($tag)) { echo $tag; } else { echo 'h3'; } ?>>
+</<?php if(isset($el)) { echo $el; } else { echo 'h3'; } ?>>
