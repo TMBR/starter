b1458a4802180187acf572bb8c129e5993942c7e
Galen Strasen <galen@wearetmbr.com>
2016-10-20 15:00:59 -0700

    CLEAN UP

--- wp-content/themes/tmbr/part-dupes/partials/components/media/img-bg.php
+++
@@ -1,3 +0,0 @@
-
-
-<div class="img-bg" style="background-image: url(<?php echo $img['sizes'][$size]; ?>);"></div>
--- wp-content/themes/tmbr/partials/flex/layout/text-block.php
+++
@@ -1,5 +0,0 @@
-<div class="flex-content text-block flex-section <?php the_sub_field('align_text'); ?>">
-	<h4 class="subtitle"><?php echo get_sub_field('section_title'); ?></h4>
-	<?php the_sub_field('section_copy'); ?>
-	<div class="clear"></div>
-</div><!-- /end center text -->
