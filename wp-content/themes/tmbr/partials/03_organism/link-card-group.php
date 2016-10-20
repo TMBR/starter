d064bb6bd7c8bbb529cfe87db94dc65f8ffe93e6
Galen Strasen <galen@wearetmbr.com>
2016-10-20 12:41:55 -0700

    Link Card Group layout

--- wp-content/themes/tmbr/partials/03_organism/link-card-group.php
+++ wp-content/themes/tmbr/partials/03_organism/link-card-group.php
@@ -12,10 +12,38 @@ global $post;
  *
  *  Dependent upon:
  *   partials/02_molecule/link-card.php
+ *
+ *  Used by:
+ *   partials/03_organism/get-flex-layouts.php
  */
 
+/* Clone fields display type */
+if(isset($display))
+{
 
-$group_title = isset($group_title) ? $group_title : get_field('link_group_header');
+  if($display == 'group' && !empty($label)) {
+    // this will likely not be the case
+  }
+
+  elseif($display == 'seamless') {
+    $group_title = isset($group_title) ? $group_title : get_sub_field('link_group_header');
+    $label1 = isset($label1) ? $label1 : get_sub_field('link_card1');
+    $label2 = isset($label2) ? $label2 : get_sub_field('link_card2');
+    $label3 = isset($label3) ? $label3 : get_sub_field('link_card3');
+  }
+
+}
+
+/* Assumes that field is getting loaded directly */
+else
+{
+  $group_title = isset($group_title) ? $group_title : get_field('link_group_header');
+  $label1 = isset($label1) ? $label1 : get_field('link_card1');
+  $label2 = isset($label2) ? $label2 : get_field('link_card2');
+  $label3 = isset($label3) ? $label3 : get_field('link_card3');
+}
+
+
 ?>
 
 
@@ -26,28 +54,25 @@ $group_title = isset($group_title) ? $group_title : get_field('link_group_header
     <div class="row">
       <div class="col-md-4 col-xs-12">
         <?php
-        $label = get_field('link_card1');
         tmbr_load_template( 'partials/02_molecule/link-card.php', array(
           'display' => 'group',
-          'label' => (isset($label)) ? $label : null
+          'label' => (isset($label1)) ? $label1 : null
         ));
         ?>
       </div><!-- /col -->
       <div class="col-md-4 col-xs-12">
         <?php
-        $label = get_field('link_card2');
         tmbr_load_template( 'partials/02_molecule/link-card.php', array(
           'display' => 'group',
-          'label' => (isset($label)) ? $label : null
+          'label' => (isset($label2)) ? $label2 : null
         ));
         ?>
       </div><!-- /col -->
       <div class="col-md-4 col-xs-12">
         <?php
-        $label = get_field('link_card3');
         tmbr_load_template( 'partials/02_molecule/link-card.php', array(
           'display' => 'group',
-          'label' => (isset($label)) ? $label : null
+          'label' => (isset($label3)) ? $label3 : null
         ));
         ?>
       </div><!-- /col -->
