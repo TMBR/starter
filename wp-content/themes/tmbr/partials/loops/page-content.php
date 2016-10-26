2e009498857616dd44c3fc44723502a838c78c97
Galen Strasen <galen@wearetmbr.com>
2016-10-25 17:21:44 -0700

    Page content loop

--- wp-content/themes/tmbr/partials/loops/page-content.php
+++ wp-content/themes/tmbr/partials/loops/page-content.php
@@ -5,23 +5,33 @@
  *
  */
 ?>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 itemprop="headline" class="entry-title animation" data-animation="animation-fade-in-down">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header hidden">
+				<?php the_title( '<h2 itemprop="headline" class="entry-title animation" data-animation="animation-fade-in-down">', '</h2>' ); ?>
+				</header><!-- .entry-header -->
 
-	<div itemprop="text" class="entry-content animation" data-animation="animation-fade-in-up">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+				<div itemprop="text" class="entry-content animation" data-animation="animation-fade-in-up">
+					<?php
+					the_content();
 
-	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+					wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
+					'after'  => '</div>',
+					) );
+
+					?>
+				</div><!-- .entry-content -->
+
+				<footer class="entry-footer">
+					<?php edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
+				</footer><!-- .entry-footer -->
+
+			</article><!-- #post-## -->
+
+		</div><!-- /col -->
+	</div><!-- /row -->
+</div><!-- /container -->
